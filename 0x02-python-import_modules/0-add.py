b36e02ad412a0d6df86b8a010f682213518f28ed
John Iluyemi <johniluyemi@gmail.com>
2022-09-23 12:10:37 +0100

    submit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import add_0
-a = 1
-b = 2
-print(f"{a:d} + {b:d} = {add_0.add(a, b):d}")
+if __name__ == "__main__":
+    import add_0
+    a = 1
+    b = 2
+    print(f"{a} + {b} = {add_0.add(a, b)}")
