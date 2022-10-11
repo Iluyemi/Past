58cfd856efc02ddcbd061b70ad0b542c3209bbf5
* <johniluyemi@gmail.com>
2022-10-11 23:58:11 +0100

    whitespace

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end="")
-            a+=1
+            a += 1
         print("")
         return a
     except IndexError:
