292aa26463a8a761a2f136e85eb57a821c8094f4
John Iluyemi <johniluyemi@gmail.com>
2022-10-12 20:53:28 +0000

    restructuring

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-"""Print an integer safely."""
-
 
 def safe_print_integer(value):
-    """Print an integer safely."""
-    try:
-        print("{:d}".format(value))
-    except BaseException:
-        return False
-    return True
+	try:
+		print("{:d}".format(value))
+	except BaseException:
+		return False
+	return True
