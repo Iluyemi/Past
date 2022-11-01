a692d4acbe224bd07363d6ec50cd3fee9158b093
Iluyemi John <johniluyemi@gmail.com>
2022-11-01 07:29:19 +0000

    done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-
+"""My rectangle class"""
 from models.base import Base
 
 
 class Rectangle(Base):
+	"""inheriting from base class"""
+
+
 	def __init__(self, width, height, x=0, y=0, id=None):
-        	Base.__init__(self, id)
-            	self.__height = height 
-            	self.__width = width 
-            	self.__x = x 
-            	self.__y = y
+		"""initialization function"""
+		self.width = width
+		self.height = height 
+		self.x = x
+		self.y = y
+		super().__init__(id)
