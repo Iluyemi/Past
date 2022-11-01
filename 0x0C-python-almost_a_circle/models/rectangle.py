6da41962e4dd07e88a894d33984617ccb5eb75d2
Iluyemi John <johniluyemi@gmail.com>
2022-11-01 07:18:07 +0000

    done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,11 +2,11 @@
 
 from models.base import Base
 
-if __name__ == "__main__":
-    class Rectangle(Base):
-        def __init__(self, width, height, x=0, y=0, id=None):
-            Base.__init__(self, id)
-            self.__height = height 
-            self.__width = width 
-            self.__x = x 
-            self.__y = y
+
+class Rectangle(Base):
+	def __init__(self, width, height, x=0, y=0, id=None):
+        	Base.__init__(self, id)
+            	self.__height = height 
+            	self.__width = width 
+            	self.__x = x 
+            	self.__y = y
