370cbea567783d24f4f6e996fa7bf177d8099888
root <root@Johnnie.localdomain>
2022-11-04 08:40:33 +0100

    submit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,40 +1,53 @@
 #!/usr/bin/python3
-"""My rectangle class"""
+"""rectangle class"""
 from models.base import Base
 
 
 class Rectangle(Base):
-	"""inheriting from base class"""
+    """The rectsangle class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
 
+    @property
+    def height(self):
+        """height getter function"""
+        return self.__height
 
-	def __init__(self, width, height, x=0, y=0, id=None):
-		"""initialization function"""
-		self.width = width
-		self.height = height 
-		self.x = x
-		self.y = y
-		super().__init__(id)
-	@property
-	def height(self):
-		"""getter for height"""
-		return(self.__height)
-	@property
-	def width(self):
-		"""getter for height"""
-                return(self.__width)
-	@property
-	def x(self):
-		"""getter for x"""
-		return(self.__x)
-	@property
-	def y(self):
-		 """getter for y"""
-		return(self.__y)
-	@height.setter
-	def height(self, value):
-		""" setter for height"""
-		self.__value = value
-	def width(self, value):
-		"""setter for width"""
-		self.__value = value
-	
+    @property
+    def width(self):
+        """width getter function"""
+        return self.__width
+
+    @property
+    def x(self):
+        """x getter function"""
+        return self.__x
+
+    @property
+    def y(self):
+        """y getter function"""
+        return self.__y
+
+    @height.setter
+    def height(self, height):
+        """height getter"""
+        self.__height = height
+
+    @width.setter
+    def width(self, width):
+        """width setter"""
+        self.__width = width
+
+    @x.setter
+    def x(self, x):
+        """x setter"""
+        self.__x = x
+
+    @y.setter
+    def y(self, y):
+        """y setter"""
+        self.__y = y
